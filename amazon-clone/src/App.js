77a5099b8dca6854f939350fb5b0ff5e1e361c8d
Juan Hoyos <juanse.jsh@gmail.com>
2020-10-01 00:49:51 -0400

    header component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import Header from "./Components/Header";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Amazon Clone In Construction </h2>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>login</h1>
+          </Route>
+          {/*this is the default page*/}
+          <Route>
+          <Header/>
+            <h1>Home Page</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ amazon-clone/src/Components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingCart"
+import "./Header.css";
+
+function Header(){
+  return (
+    <nav className="header">
+    {/*Logo on the left->img
+        Search box
+        SignIn
+        Returns and orders
+        Your Prime
+        Basket icon with a number*/}
+        <Link to="/">
+          <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="Amazon"/>
+        </Link>
+        <div className="header_search">
+          <input className="header_searchInput" type="text"/>
+          <SearchIcon className="header_searchIcon"/>
+        </div>
+        <div className="header_nav">
+          <Link className="header_link" to="/login">
+          <div className="header_option">
+            <span className="header_optionLineOne">hello Juan</span>
+            <span className="header_optionLineTwo">Sign In</span>
+          </div>
+          </Link>
+          <Link className="header_link" to="/">
+          <div className="header_option">
+            <span className="header_optionLineOne">Returns</span>
+            <span className="header_optionLineTwo">& Orders</span>
+          </div>
+          </Link>
+          <Link className="header_link" to="/">
+          <div className="header_option">
+            <span className="header_optionLineOne">Your</span>
+            <span className="header_optionLineTwo">Prime</span>
+          </div>
+          </Link>
+          <Link className="header_link" to="/checkout">
+          <div className="header_optionBasket">
+            <ShoppingBasketIcon className="header_shoppingIcon"/>
+            <span className="header_optionLineTwo header_basketCount">0</span>
+          </div>
+          </Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
